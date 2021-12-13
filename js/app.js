d1c24e3084d02eb9dcb89f0db3b776c21867e632
zVuDa <mgoncao.sansus3@gmail.com>
2021-12-13 10:44:00 +0100

    inicio

---
+++ js/app.js
@@ -0,0 +1,6 @@
+/**
+ * document.querySelector() --> Selección de nodos de tipo object HTMLElement
+ * document.createElement() --> creación de nodos de tipo object HTMLElement
+ * document.createTextNode() --> creación de nodos de tipo object node text
+ * element.className = 'nombre-clase' // element.setAttribute('class', 'nombre-clase') --> poner nombre clase de un atributo // element.classList.add('nombre-clase)
+ */
